cea7bb4b5135739e8fb93ff6fe93534082c47b55
Dale Pope <dale@dalepope.name>
2011-07-29 18:59:41 -0600

    Added Help page

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ SampleApp::Application.routes.draw do
 
   get "pages/contact"
   
-  get "pages/about"
+  get "pages/help"
 
+  get "pages/about"
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -29,6 +29,19 @@ describe PagesController do
     end
   end
 
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+	                    :content => "Ruby on Rails Tutorial Sample App | Help")
+    end
+  end
+
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
