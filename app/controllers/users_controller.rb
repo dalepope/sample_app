88f92ee0711a6aa99f238eb58899d18d9977a2e1
Dale Pope <dale@dalepope.name>
2011-08-08 17:29:42 -0600

    Editing a user now requires sign in but does not prevent users from editing each other.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -40,4 +41,9 @@ class UsersController < ApplicationController
     end
   end
   
+  private
+  
+    def authenticate
+      deny_access unless signed_in?
+    end
 end
